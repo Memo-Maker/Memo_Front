263b297b020737fe4352c8615144d7db31bb2bb6
taeksin <yoyo2521@naver.com>
2024-03-18 19:58:10 +0900

    Header 수정

--- memo_front/src/App.js
+++ memo_front/src/App.js
@@ -1,20 +1,21 @@
 //import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header"
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      </header>
-    </div>
+      <div className="App">
+        {/* 헤더 컴포넌트를 추가합니다. */}
+        <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+      </div>
     </Router>
   );
 }
---
+++ memo_front/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My App</h1>
+      {/* Add any other header content here */}
+    </header>
+  );
+}
+
+export default Header;
