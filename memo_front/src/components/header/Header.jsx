8d5c98bf6f6bddd7ba4a2ef172f27d044c6f5fdc
shawn_its <psh2968@naver.com>
2024-03-25 04:35:26 +0900

    Header 보수

--- memo_front/src/components/header/Header.jsx
+++ memo_front/src/components/header/Header.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Logo from "../../assets/images/logo.png";
+import Search from "../../assets/images/search.png";
+import Profile from "../../assets/images/profile.png";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1vw 2vw;
+`;
+
+const Left = styled.div`
+  display: flex;
+  gap: 1vw;
+`;
+
+const Right = styled.div`
+  display: flex;
+  gap: 3vw;
+`;
+
+const LogoTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HamburgerButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 2vw;
+`;
+const SearchTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProfileTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 function Header() {
   return (
-    <header>
-      <h1>Welcome to My App</h1>
-      {/* Add any other header content here */}
-    </header>
+    <HeaderContainer>
+      <Left>
+      <HamburgerButton>☰</HamburgerButton>
+      <LogoTitle>
+        <img src={Logo} alt="Logo" />
+        </LogoTitle>
+      </Left>
+      <Right>
+      <SearchTitle>
+        <img src={Search} alt="Search" />
+      </SearchTitle>
+      <ProfileTitle>
+        <img src={Profile} alt="Profile" />
+        </ProfileTitle>
+      </Right>
+    </HeaderContainer>
   );
 }
 
--- memo_front/src/pages/HomePage.jsx
+++ memo_front/src/pages/HomePage.jsx
@@ -1,18 +1,52 @@
-//첫 화면
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import styled from "styled-components";
 
+// 스타일 컴포넌트 정의
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f0f0;
 `;
 
+const Title = styled.h1`
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #333;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+// HomePage 컴포넌트
 const HomePage = () => {
   return (
     <Container>
-      <h1>정리할 영상의 링크를 걸어주세요!</h1>
+      <Title>정리한 영상의 링크를 걸어주세요!</Title>
+      <Input
+        type="text"
+        placeholder="학습하고 싶은 YouTube 영상의 링크를 붙여넣어주세요"
+      />
+      <Button>올리기</Button>
     </Container>
   );
 };
