31cb217d4429e75c2db6cb5c2830f6928c5f2143
Taeksin Kim <yoyo2521@naver.com>
2024-04-10 12:17:17 +0900

    로컬저장하는걸 Auth로 옮김

--- memo_front/src/context/AuthContext.jsx
+++ memo_front/src/context/AuthContext.jsx
@@ -160,16 +160,18 @@ export const AuthProvider = ({ children }) => {
   };
 
 
-  const saveContentToLocalStorage = (htmlContent)=>{
+  const saveContentToLocal = (htmlContent)=>{
   localStorage.setItem("editorContent", htmlContent)
   console.log("텍스트 내용이 로컬스토리지에 저장되었씁니다.")
 };
+
   return (
     <AuthContext.Provider
       value={{
         isLoggedIn,
         user,
         getTokenFromLocalStorage,
+        saveContentToLocal,
         signup,
         login,
         logout,
