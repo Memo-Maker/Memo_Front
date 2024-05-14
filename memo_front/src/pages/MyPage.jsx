abc341120ea32812b0c2dc110e796d25c8301ef6
Taeksin Kim <yoyo2521@naver.com>
2024-05-15 03:30:49 +0900

    MyPage 영상 우클릭시 삭제 모달 UI구현

--- memo_front/src/pages/MyPage.jsx
+++ memo_front/src/pages/MyPage.jsx
@@ -2,6 +2,67 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "../context/AuthContext";
 
+// 모달창 배경
+const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+`;
+
+// 모달창 내용
+const ModalContent = styled.div`
+  background-color: #c2c2c2;
+  /* padding: 2rem; */
+  border-radius: 0.5rem;
+  width: 20%;
+  text-align: center;
+`;
+
+const Button = styled.div`
+  background-color: #c2c2c2;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 0.5rem;
+`;
+
+const CancelButton = styled.button`
+  width: 98%;
+  padding: 0.5rem 1rem;
+  margin-right: 4px;
+  border: none;
+  cursor: pointer;
+  background-color: #adadad;
+  color: #000000;
+  font-size: 1rem;
+  border-bottom-left-radius: 0.3rem; /* 왼쪽 하단 모서리에만 border-radius 적용 */
+  &:hover {
+    background-color: #606060;
+  }
+`;
+
+const DeleteButton = styled.button`
+  width: 102%;
+  padding: 1vw 1vw;
+  border: none;
+  cursor: pointer;
+  background-color: #adadad;
+  color: #ff0000;
+  font-size: 1rem;
+  border-bottom-right-radius: 0.3rem; /* 오른쪽 하단 모서리에만 border-radius 적용 */
+
+  &:hover {
+    background-color: #606060;
+  }
+`;
+
+
 // 그리드 형식으로 카드들을 표시하는 컨테이너
 const GridContainer = styled.div`
   display: grid;
@@ -98,9 +159,12 @@ const itemsPerPage = 12;
 
 const MyPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const {selectVideo } = useAuth();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const { selectVideo } = useAuth();
   const videoList = JSON.parse(localStorage.getItem("videoList")) || [];
   const categoryName = localStorage.getItem("categoryName");
+
   // 현재 페이지에 따른 데이터 계산
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -140,15 +204,34 @@ const MyPage = () => {
     return pages;
   };
 
+  const openModal = (video) => {
+    setSelectedVideo(video);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedVideo(null);
+  };
+
+  const handleContextMenu = (event, video) => {
+    event.preventDefault();
+    openModal(video);
+  };
+
   return (
     <>
-    {categoryName} 카테고리의 영상
+      {categoryName} 카테고리의 영상
       <GridContainer>
         {currentData.map((video, index) => (
-          <StyledButton key={index} onClick={() => selectVideo(video.videoUrl)}>
-          <ButtonImage src={video.thumbnailUrl} />
-          <ButtonContent>{video.videoTitle}</ButtonContent>
-        </StyledButton>
+          <StyledButton
+            key={index}
+            onClick={() => selectVideo(video.videoUrl)}
+            onContextMenu={(e) => handleContextMenu(e, video)}
+          >
+            <ButtonImage src={video.thumbnailUrl} />
+            <ButtonContent>{video.videoTitle}</ButtonContent>
+          </StyledButton>
         ))}
       </GridContainer>
       <PaginationContainer>
@@ -163,6 +246,23 @@ const MyPage = () => {
           {">"}
         </NextButton>
       </PaginationContainer>
+      {showModal && (
+        <ModalBackdrop onClick={closeModal}>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            <p>이 영상을 삭제하시겠습니까?</p>
+            <Button>
+              <CancelButton onClick={closeModal}>취소</CancelButton>
+              <DeleteButton
+                onClick={() => {
+                  /* 삭제 함수 호출 */ closeModal();
+                }}
+              >
+                삭제
+              </DeleteButton>
+            </Button>
+          </ModalContent>
+        </ModalBackdrop>
+      )}
     </>
   );
 };
