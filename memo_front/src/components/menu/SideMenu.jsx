d64b10808e60fea9d09ee90675d4be2a64723c6d
Taeksin Kim <yoyo2521@naver.com>
2024-05-15 03:47:11 +0900

    SideMenu UI수정

--- memo_front/src/components/menu/SideMenu.jsx
+++ memo_front/src/components/menu/SideMenu.jsx
@@ -1,14 +1,12 @@
-// SideMenu 컴포넌트
-
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "../../context/AuthContext";
+import SaveModal from "../modal/SaveModal"; // SaveModal을 불러옵니다.
 import folderIcon from "../../assets/images/macos_folder.png";
 
 const SideMenuContainer = styled.div`
   position: fixed;
-  bottom: ${({ isOpen }) =>
-    isOpen ? "0" : "-100%"}; /* isOpen 상태에 따라 아래로 내려옴/올라감 */
+  bottom: ${({ isOpen }) => (isOpen ? "0" : "-100%")}; /* isOpen 상태에 따라 아래로 내려옴/올라감 */
   left: 0;
   width: 16%;
   height: 80%;
@@ -29,6 +27,12 @@ const CloseButton = styled.button`
   cursor: pointer;
 `;
 
+const CategoryContainer = styled.div`
+  max-height: 90%; /* 높이 제한을 설정하여 스크롤이 필요한 경우 스크롤바가 나타나도록 함 */
+  overflow-y: auto;
+  margin-bottom: 1rem;
+`;
+
 const StyledMenuItem = styled.div`
   display: flex;
   width: 80%;
@@ -43,6 +47,7 @@ const StyledMenuItem = styled.div`
     background-color: #f0f0f0; /* 마우스를 올렸을 때 배경색 변경을 위한 스타일을 추가합니다. */
   }
 `;
+
 const Icon = styled.img`
   width: 1.5vw;
   margin-right: 1vw;
@@ -54,7 +59,25 @@ const Text = styled.div`
   text-align: start; /* 텍스트 중앙 정렬 */
 `;
 
+const EditButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 3vw;
+  margin: 1vw 0 1vw 1.2vw;
+  border: 2px dashed #d9d9d9;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
 const SideMenu = ({ isOpen, onClose }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 관리
   const categoryList = JSON.parse(localStorage.getItem("categoryList")) || []; // 로컬스토리지에서 카테고리 목록 가져오기
   const { getVideoList } = useAuth();
 
@@ -64,19 +87,36 @@ const SideMenu = ({ isOpen, onClose }) => {
     getVideoList(category);
   };
 
+  const handleEditCategories = () => {
+    setIsModalOpen(true); // 모달을 엽니다
+    console.log("카테고리 수정 버튼 클릭됨");
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); // 모달을 닫습니다
+  };
+
   return (
-    <SideMenuContainer isOpen={isOpen}>
-      <CloseButton onClick={onClose}>×</CloseButton>
-      {categoryList.map((category, index) => (
-        <StyledMenuItem
-          key={index}
-          onClick={() => handleMenuItemClick(category)}
-        >
-          <Icon src={folderIcon} alt="Folder Icon" />
-          <Text>{category}</Text>
-        </StyledMenuItem> // 각 카테고리에 대한 메뉴 항목 생성
-      ))}
-    </SideMenuContainer>
+    <>
+      <SideMenuContainer isOpen={isOpen}>
+        <CloseButton onClick={onClose}>×</CloseButton>
+        <CategoryContainer>
+          {categoryList.map((category, index) => (
+            <StyledMenuItem
+              key={index}
+              onClick={() => handleMenuItemClick(category)}
+            >
+              <Icon src={folderIcon} alt="Folder Icon" />
+              <Text>{category}</Text>
+            </StyledMenuItem> // 각 카테고리에 대한 메뉴 항목 생성
+          ))}
+        </CategoryContainer>
+        <EditButton onClick={handleEditCategories}>
+          + 카테고리 수정
+        </EditButton>
+      </SideMenuContainer>
+      {isModalOpen && <SaveModal closeModal={closeModal} />} {/* 모달을 조건부로 렌더링 */}
+    </>
   );
 };
 
