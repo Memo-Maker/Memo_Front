4b2808128f525837996fcd71ffd442495bbe66fa
taeksin <yoyo2521@naver.com>
2024-05-14 15:54:39 +0900

    sideMenu UI변경

--- memo_front/src/components/menu/SideMenu.jsx
+++ memo_front/src/components/menu/SideMenu.jsx
@@ -3,13 +3,14 @@
 import React from "react";
 import styled from "styled-components";
 import { useAuth } from "../../context/AuthContext";
+import folderIcon from "../../assets/images/macos_folder.png";
 
 const SideMenuContainer = styled.div`
   position: fixed;
   bottom: ${({ isOpen }) =>
     isOpen ? "0" : "-100%"}; /* isOpen 상태에 따라 아래로 내려옴/올라감 */
   left: 0;
-  width: 25%;
+  width: 16%;
   height: 80%;
   background-color: #ffffff;
   box-shadow: 2px 0 10px rgba(0, 0, 0, 0.1);
@@ -29,17 +30,28 @@ const CloseButton = styled.button`
 `;
 
 const StyledMenuItem = styled.div`
-  width: 100%;
+  display: flex;
+  width: 80%;
   border-radius: 10px; /* 둥근 네모 모양의 버튼으로 만들기 위해 border-radius 값을 조정합니다. */
-  padding: 20px;
-  border: 1px solid transparent; /* 테두리를 추가하여 버튼처럼 보이도록 설정합니다. */
-  border-bottom: 1px solid #e0e0e0; /* 각 메뉴 사이에 구분선 추가 */
+  margin: 1vw 0 1vw 1.2vw;
+  padding: 1vw 0 1vw 0;
+  border: 2px solid #d9d9d9; /* 테두리를 추가하여 버튼처럼 보이도록 설정합니다. */
   cursor: pointer; /* 마우스 포인터를 올렸을 때 버튼임을 나타내기 위해 커서 모양을 변경합니다. */
   transition: background-color 0.3s; /* 배경색 변경에 대한 애니메이션 효과를 추가합니다. */
 
   &:hover {
     background-color: #f0f0f0; /* 마우스를 올렸을 때 배경색 변경을 위한 스타일을 추가합니다. */
   }
+`;
+const Icon = styled.img`
+  width: 1.5vw;
+  margin-right: 1vw;
+  margin-left: 1vw;
+`;
+
+const Text = styled.div`
+  flex: 1; /* 텍스트가 남은 공간을 차지하도록 */
+  text-align: start; /* 텍스트 중앙 정렬 */
 `;
 
 const SideMenu = ({ isOpen, onClose }) => {
@@ -56,8 +68,12 @@ const SideMenu = ({ isOpen, onClose }) => {
     <SideMenuContainer isOpen={isOpen}>
       <CloseButton onClick={onClose}>×</CloseButton>
       {categoryList.map((category, index) => (
-        <StyledMenuItem key={index} onClick={() => handleMenuItemClick(category)}>
-          {category}
+        <StyledMenuItem
+          key={index}
+          onClick={() => handleMenuItemClick(category)}
+        >
+          <Icon src={folderIcon} alt="Folder Icon" />
+          <Text>{category}</Text>
         </StyledMenuItem> // 각 카테고리에 대한 메뉴 항목 생성
       ))}
     </SideMenuContainer>
